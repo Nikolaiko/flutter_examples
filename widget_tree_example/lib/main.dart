894a8168749ff83019ee6a398671c45e186f7fdf
Nikolai <nbaklanov@issart.com>
2020-11-06 20:38:28 +0600

    Added initialization

--- widget_tree_example/lib/main.dart
+++ widget_tree_example/lib/main.dart
@@ -5,6 +5,7 @@ import 'package:main_test/widget_tree_example.dart';
 import 'package:main_test/widget_tree_example_rebuild.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(
     MaterialApp(
       home: Scaffold(
