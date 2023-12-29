8972d4084613042f4c447dc89c01e40360f6ca75
Nikolai Baklanov <nikolaiko@mail.ru>
2023-12-29 22:58:01 +0600

    Last.

---
+++ leet_code_tasks/android/app/src/main/kotlin/com/example/leet_code_tasks/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.leet_code_tasks
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ leet_code_tasks/lib/leet_code_tasks/arrays_101/max_consecutive_ones.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+Int maxConsecutiveOnes(List<Int>) {
+  return 3;
+}
